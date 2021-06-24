8cac35407cd1d9fc6344c6b9578b068782d1aecc
danielchang2001 <danielchang083@gmail.com>
2021-06-23 18:18:12 -0700

    Temporarily styled button

--- client/src/Login.js
+++ client/src/Login.js
@@ -5,9 +5,14 @@ import { Container } from 'react-bootstrap'
 const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=f4e7fa397aab487c9b0f37079d268b5c&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%user-library-read%20user-library-modify%20user-read-playback=state%20user-modify-playback-state"
 
 export default function Login() {
-    return <Container>
-        <a className = "btn btn-success btn-lg" href={AUTH_URL}>
-            Login with Spotify
-        </a>
-    </Container>
+    return (
+        <Container 
+        className="d-flex justify-content-center align-items-center"
+        style={{ minHeight: "100vh" }}
+        >
+            <a className = "btn btn-success btn-lg" href={AUTH_URL}>
+                Login with Spotify
+            </a>
+        </Container>
+    )
 }
