9c0a209ab7477ac74e0c09c3630912bf6053ed08
danielchang2001 <danielchang083@gmail.com>
2021-06-23 22:20:40 -0700

    Added song title and artist that accompanies the song image

--- client/src/TrackSearchResult.js
+++ client/src/TrackSearchResult.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 export default function TrackSearchResult({track}) {
     return (
-        <div className="d-flex m-2 align-itmes-center">
+        <div className="d-flex m-2 align-items-center">
             <img src={track.albumUrl} style={{height: '64px', width: '64px'}} />
+            <div className="mx-3">
+                <div> {track.title} </div>
+                <div classNAme="text-muted">{track.artist}</div>
+            </div>
         </div>
     )
 }
