4a2670e0aa8507a97ae211f971795cbff7fbad69
danielchang2001 <danielchang083@gmail.com>
2021-06-24 21:55:08 -0700

    Update server.js and create .env

--- server/server.js
+++ server/server.js
@@ -1,19 +1,21 @@
+require('dotenv').config()
 const express = require('express');
 const cors = require("cors")
-const bodyParser = require('body-parser')
-const spotifyWebApi = require('spotify-web-api-node');
+const spotifyWebApi = require('spotify-web-api-node')
+const lyricsFinder = require("lyrics-finder")
+
 
 const app = express();
 app.use(cors())
 app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
 
 app.post('/refresh', (req, res) => {
     const refreshToken = req.body.refreshToken
-    console.log("hi")
     const spotifyApi = new spotifyWebApi({
-        redirectUri: 'http://localhost:3000',
-        clientId:'f4e7fa397aab487c9b0f37079d268b5c',
-        clientSecret:'00502b1dfd654b2da8a1d59165a0c8db',
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret:process.env.CLIENT_SECRET,
         refreshToken
     })
 
@@ -33,9 +35,9 @@ app.post('/refresh', (req, res) => {
 app.post('/login', (req, res) => {
     const code = req.body.code
     const spotifyApi = new spotifyWebApi({
-        redirectUri: 'http://localhost:3000',
-        clientId:'f4e7fa397aab487c9b0f37079d268b5c',
-        clientSecret:'00502b1dfd654b2da8a1d59165a0c8db'
+        redirectUri: process.env.REDIRECT_URI,
+        clientId: process.env.CLIENT_ID,
+        clientSecret:process.env.CLIENT_SECRET,
     })
 
     spotifyApi.authorizationCodeGrant(code).then(data => {
@@ -48,6 +50,11 @@ app.post('/login', (req, res) => {
         console.log(err)
         res.sendStatus(400)
     })
+})
+
+app.get('/lyrics', async(req, res) => {
+    const lyrics = (await lyricsFinder(req.query.artist, req.query.track)) || "No Lyrics Found"
+    res.json({lyrics})
 })
 
 app.listen(3001)
